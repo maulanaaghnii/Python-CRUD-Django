76596ea2e6537346fec5124e4593a3cf46268684
maulanaaghnii <maulanaaghni.contact@gmail.com>
2021-11-30 20:11:32 +0700

    init

--- crudApp/urls.py
+++ crudApp/urls.py
@@ -6,10 +6,3 @@ urlpatterns = [
     path('delete/<int:id>/', views.deleteStudent, name="delete_student"),
 ]
 
-# from django.contrib import admin
-# from django.urls import path
-# from django.urls import include  # new
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('',include('crudApp.urls')),  # new
-# ]
